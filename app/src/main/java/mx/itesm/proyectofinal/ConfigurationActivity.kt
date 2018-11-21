5376f570ef52f4f1d76df6f943912213ae90f7a1
Renato Sanchez <rsanchez@Renatos-MacBook-Pro.local>
2018-11-21 13:06:17 -0600

    inicio de switch modo enfermero

--- app/src/main/java/mx/itesm/proyectofinal/ConfigurationActivity.kt
+++ app/src/main/java/mx/itesm/proyectofinal/ConfigurationActivity.kt
@@ -2,12 +2,23 @@ package mx.itesm.proyectofinal
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Switch
+import kotlinx.android.synthetic.main.activity_configuration.*
+import kotlinx.android.synthetic.main.activity_configuration.view.*
 
 class ConfigurationActivity : AppCompatActivity() {
+
+    companion object {
+        public var isNurse = false
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_configuration)
         this.title = "Configuración"
+
+        switch1.setOnCheckedChangeListener { buttonView, isChecked ->
+            isNurse = isChecked
+        }
     }
 }
