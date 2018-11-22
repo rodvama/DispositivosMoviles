500194b01f0bf28b26aca1850910613a43d42e66
Renato Sanchez <rsanchez@Renatos-MacBook-Pro.local>
2018-11-22 02:45:19 -0600

    primer avance para enviar correo

--- app/src/main/java/mx/itesm/proyectofinal/ActivityDetail.kt
+++ app/src/main/java/mx/itesm/proyectofinal/ActivityDetail.kt
@@ -2,18 +2,23 @@ package mx.itesm.proyectofinal
 
 import Database.MedicionDatabase
 import Database.ioThread
+import android.arch.persistence.room.Database
+import android.content.Intent
 import android.graphics.BitmapFactory
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_detail.*
 
 class ActivityDetail : AppCompatActivity() {
+
+    lateinit var instanceDatabase: MedicionDatabase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
 
-        val instanceDatabase = MedicionDatabase.getInstance(this)
+        instanceDatabase = MedicionDatabase.getInstance(this)
 
         val extras = intent.extras?:return
         val id = extras.getInt(PATIENT_KEY)
@@ -49,5 +54,31 @@ class ActivityDetail : AppCompatActivity() {
             }
         }
 
+        button_enviarCorreo.setOnClickListener { v ->  sendMail(id) }
+    }
+
+    fun sendMail (id:Int) {
+        ioThread {
+            val measure = instanceDatabase.medicionDao().cargarMedicionId(id)
+            val i = Intent(Intent.ACTION_SEND)
+            i.type = "message/rfc822"
+            i.putExtra(Intent.EXTRA_EMAIL, arrayOf("recipient@example.com"))
+            i.putExtra(Intent.EXTRA_SUBJECT, "subject of email")
+
+            var text = "Fecha: " + measure.fecha +
+                    "\nIniciales: " + measure.iniciales +
+                    "\nBrazo: " + measure.brazo +
+                    "\n\nApp: \n" + "    Diastolica: " + measure.appDiastolica + "\n   Sistolica: " + measure.appSistolica +
+                    "\n\nManual: \n" + "    Diastolica: " + measure.manDiastolica + "\n   Sistolica: " + measure.manDiastolica
+
+            i.putExtra(Intent.EXTRA_TEXT, text)
+            try {
+                startActivity(Intent.createChooser(i, "Send mail..."))
+            } catch (ex: android.content.ActivityNotFoundException) {
+                Toast.makeText(applicationContext, "There are no email clients installed.", Toast.LENGTH_SHORT)
+                        .show()
+            }
+
+        }
     }
 }
